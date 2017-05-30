fd626b7f3f28bf1de14c3fc19b1e6390c1baa453
Tinus Wagner <tinus@3months.com>
2017-05-30 14:14:18 +1200

    setting up witAI config stuff

--- app/extensions/wit_extension.rb
+++ app/extensions/wit_extension.rb
@@ -10,8 +10,34 @@ class WitExtension
         # something when bot responds
       },
 
-      findRestaurants: -> (request) {
-        #
+      findTheatre: -> (request) {
+        context = request["context"]
+        entities = request["entities"]
+
+        showTime = first_entity_value(entities, "datetime") || context["showTime"]
+        movie = first_entity_value(entities, "movie") || context["movie"]
+
+        if showTime
+          context["showTime"] = showTime
+          context.delete("missingTime")
+        else
+          context["missingTime"] = true
+        end
+
+        if movie
+          context["movie"] = movie
+        end
+
+        if showTime && movie
+          theatre = search_theatres(showTime, movie)
+          context["theatre"] = theatre
+          new_context = {}
+        else
+          new_context = context
+        end
+
+        @conversation.update(context: new_context)
+        return context
       }
     }
     @client = Wit.new(access_token: ENV['WIT_SERVER_TOKEN'], actions: actions)
@@ -25,4 +51,18 @@ class WitExtension
     @conversation = conversation
   end
 
+  private
+
+  def first_entity_value(entities, entity)
+    return nil unless entities.has_key? entity
+    val = entities[entity][0]['value']
+    return nil if val.nil?
+    return val.is_a?(Hash) ? val['value'] : val
+  end
+
+  def search_theatres(showTime, movie)
+    # some cool search
+    puts 'Searching for theatre...'
+    return 'Random Theatre'
+  end
 end
