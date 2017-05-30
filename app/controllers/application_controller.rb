9566d590a72e62d5d53493b263e12d69ddfd13bb
Tinus Wagner <tinus@3months.com>
2017-05-30 13:35:47 +1200

    overriding serializer default behaviour and allowing cors from any source, nice and insecure for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  include ActionController::ImplicitRender
+  respond_to :json
+
+  rescue_from RailsParam::Param::InvalidParameterError do |exception|
+    render json: {errors: exception}, status: 422
+  end
+
+  def defaul_serializer_options
+    {root: false}
+  end
+
 end
